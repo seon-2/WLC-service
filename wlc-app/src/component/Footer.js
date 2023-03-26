e0491d0a0f55ef2e211ce99e182be260a9f38cfc
gksrlffla <gksrlffla@naver.com>
2023-03-26 20:39:32 +0900

    저작권안내추가

--- wlc-app/src/component/Footer.js
+++ wlc-app/src/component/Footer.js
@@ -5,6 +5,9 @@ class Footer extends Component {
 		return(
 			<footer id="footer">
 				<div className="ft_logo"></div>
+				<div className="copyright">
+					<p>저작권 안내 : 웨스트민스터 대요리문답 196개 문답의 본문 구조 및 구문 분석은 "흑곰북스" 출판사 고유의 것으로서, 신학적인 변질과 오용을 막기 위해 복제나 수정을 금지합니다.</p>
+				</div>
 				<div className="link">
 					<a href="https://moonlightchurch.modoo.at" target="_blank">
 						<i className="fa-regular fa-house"></i>
